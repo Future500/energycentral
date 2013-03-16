b7052c28cd995e48e60d8cfb2d98b5f389b04f65
Ramon de la Fuente <ramon@future500.nl>
2013-03-16 11:22:44 +0100

    reindented code

--- bluetooth/sma_struct.h
+++ bluetooth/sma_struct.h
@@ -20,16 +20,16 @@
 #endif
 
 typedef struct{
-    char Inverter[20]; 		/*--inverter 	-i 	*/
-    char Serial[20]; 		/*derived               */
-    char BTAddress[20];         /*--address  	-a 	*/
-    int  bt_timeout;		/*--timeout  	-t 	*/
-    char Password[20];          /*--password 	-p 	*/
-    char Config[80];            /*--config   	-c 	*/
-    char File[80];              /*--file     	-f 	*/
-    float latitude_f;           /*--latitude  	-la 	*/
-    float longitude_f;          /*--longitude 	-lo 	*/
-    char Setting[80];           /*inverter model data*/
-    unsigned char InverterCode[4]; /*Unknown code inverter specific*/
-    unsigned int ArchiveCode;    /* Code for archive data */
+    char Inverter[20];              /*--inverter 	-i 	    */
+    char Serial[20];                /*derived             */
+    char BTAddress[20];             /*--address  	-a 	    */
+    int  bt_timeout;		            /*--timeout  	-t 	    */
+    char Password[20];              /*--password 	-p 	    */
+    char Config[80];                /*--config   	-c 	    */
+    char File[80];                  /*--file     	-f 	    */
+    float latitude_f;               /*--latitude  	-la 	*/
+    float longitude_f;              /*--longitude 	-lo 	*/
+    char Setting[80];               /*inverter model data*/
+    unsigned char InverterCode[4];  /*Unknown code inverter specific*/
+    unsigned int ArchiveCode;       /* Code for archive data */
 } ConfType;
