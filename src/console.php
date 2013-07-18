f605d4be8ceb5bad5ddb236f4356d1f0ccb2ec4a
Ramon de la Fuente <ramon@future500.nl>
2013-07-18 09:30:20 +0200

    cleanup of old index/config files

--- src/console.php
+++ src/console.php
@@ -6,15 +6,36 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
-$console = new Application('LogCentral', 'n/a');
+$console = new Application('EnergyCentral', 'n/a');
 
-// $console
-//     ->register('testevent')
-//     ->setDescription('Send a test event to localhost')
-//     ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
-                // Some Code Here;
-//         );
-//     })
-// ;
+$console
+    ->register('demo:greet')
+    ->setDescription('Greet someone')
+    ->addArgument(
+        'year',
+        InputArgument::OPTIONAL,
+        'What year do you want to '
+    )
+    // ->addOption(
+    //    'yell',
+    //    null,
+    //    InputOption::VALUE_NONE,
+    //    'If set, the task will yell in uppercase letters'
+    // )
+    ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
+            $name = $input->getArgument('year');
+            if ($name) {
+                $text = 'Hello '.$name;
+            } else {
+                $text = 'Hello';
+            }
+
+            if ($input->getOption('yell')) {
+                $text = strtoupper($text);
+            }
+
+            $output->writeln($text);
+    })
+;
 
 return $console;
