ed115b774bc297d45a2e362565857369a96bf426
Robbin <86378@ict-idcollege.nl>
2013-07-22 12:42:36 +0200

    Changed graph so it uses AJAX calls

--- src/controllers.php
+++ src/controllers.php
@@ -8,15 +8,16 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 $app->get('/', function (Request $request) use ($app) { // fetch a day (it will use the current day by default)
     return $app['twig']->render('index.html', array('dayStats' => $app['stats']->fetchDayHighcharts($app, ''),
-                                                    'validDate' => true,
                                                     'date' => date('d-m-Y')));
 });
 
 $app->get('/{date}', function (Request $request, $date) use ($app) { // fetch a day or month with a specified date
-    if(strlen($date) > 7) { // we've chosen a day (ex: 2013-04-07)
-        return $app['twig']->render('index.html', array('dayStats' => $app['stats']->fetchDayHighcharts($app, $date),
+    if (strlen($date) > 7) { // we've chosen a day (ex: 2013-04-07)
+      /*  return $app['twig']->render('index.html', array('dayStats' => $app['stats']->fetchDayHighcharts($app, $date),
                                                         'validDate' => $app['stats']->checkDate($date),
-                                                        'date' => $date));
+                                                        'date' => $date));*/
+
+        return $app['stats']->fetchDayHighcharts($app, $date);
     } else { // we've chosen a month (ex: 2013-04)
 
     }
