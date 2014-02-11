ada110cb727db53cdf77c52565cbd14cc9feb5c3
Robbin <86378@ict-idcollege.nl>
2014-02-11 09:52:36 +0100

    Stats class cleanup

--- src/EC/Stats.php
+++ src/EC/Stats.php
@@ -17,43 +17,44 @@ class Stats
         return $dateRes != false; // check if it's a valid date
     }
 
+    private function getJsTimestamp($time)
+    {
+        return strtotime($time . 'UTC') * 1000; // Javascript timestamps are in miliseconds
+    }
+
     private function encodeHighcharts(array $input, $months = false)
     {
-        $numberArray = array();
+        $encodedData = array();
 
         if (count($input)) { // If there is any data then we loop through it, if there isn't then the graph will be empty
-            if (!$months) { // not needed when only looping through months
-                // We start with 0 kW so we take the first index of our input (which is the first time energy is received) and subtract 5 minutes
-                $firstReading = strtotime($input[0]['datetime'] . 'UTC') * 1000;
-                array_push($numberArray, array($firstReading - 300000, 0.0)); // We start with 0 kW 5 minutes before the first reading
-            }
-
-            // Save time and energy production to array
-            foreach ($input as $row) {
-                $rowName = ($months ? $row['date'] : $row['datetime']); // date or datetime depends on if we are getting only months or only days
-                array_push($numberArray, array(strtotime($rowName . 'UTC') * 1000, floatval($row['kW'])));
-            }
-
+            $dateType = $months ? 'date' : 'datetime'; // date or datetime?
             $lastIndex = count($input) - 1; // Amount of elements in array
+            $firstTime = $this->getJsTimestamp($input[0][$dateType]); // first recorded time in the array
+            $lastTime = $this->getJsTimestamp($input[$lastIndex][$dateType]); // last recorded time in the array
 
             if (!$months) { // not needed when only looping through months
-                // We end with 0 kW so we take the last index of our input (which is the last time energy is received) and add 5 minutes
-                $lastTime = strtotime($input[$lastIndex]['datetime'] . 'UTC') * 1000;
-                array_push($numberArray, array($lastTime + 300000, 0.0)); // We end with 0 kW 5 minutes after the last reading
+                array_push($encodedData, array($firstTime - 300000, 0.0)); // We start with 0 kW 5 minutes before the first reading
+            }
+
+            foreach ($input as $row) {
+                array_push($encodedData, array($this->getJsTimestamp($row[$dateType]), floatval($row['kW']))); // Save time and energy production to array
+            }
+
+            if (!$months) { // not needed when only looping through months
+                array_push($encodedData, array($lastTime + 300000, 0.0)); // We end with 0 kW, 5 minutes after the last reading
             } else { // we are looping months, fill up the days if they don't have any data yet
-                $datetime = new \DateTime($input[$lastIndex]['date']);
-                $lastTime = strtotime($input[$lastIndex]['date'] . 'UTC') * 1000;
+                $datetime = new \DateTime($input[$lastIndex][$dateType]);
                 $dayDiff = cal_days_in_month(CAL_GREGORIAN, $datetime->format('m'), $datetime->format('y'))  - count($input); //calculate day difference for a full month
 
                 if ($dayDiff > 0) {
                     for ($i = 0; $i < $dayDiff; $i++) { // add days until the month is filled
-                        $lastTime = $lastTime + (24 * 3600 * 1000);
-                        array_push($numberArray, array($lastTime, 0.0));
+                        $lastTime += (24 * 3600 * 1000);
+                        array_push($encodedData, array($lastTime, 0.0));
                     }
                 }
             }
         }
-        return json_encode($numberArray); // encode for jquery
+        return json_encode($encodedData); // encode for jquery
     }
 
     public function fetchDay(Silex\Application $app, $date = null, $trimZeroData = false)
@@ -64,7 +65,7 @@ class Stats
         }
 
         // Setup return variable
-        $retn = array();
+        $dayData = array();
 
         // Check if valid date otherwise we can skip the next part of code
         if ($this->checkDate($date)) {
@@ -81,19 +82,19 @@ class Stats
 
                 // Check if results aren't empty and also show 0 values
                 if (!empty($beginGraph) && !empty($endGraph)) {
-                    $retn = $app['db']->fetchAll(
+                    $dayData = $app['db']->fetchAll(
                         "SELECT * FROM daydata WHERE datetime >= :first AND datetime <= :last",
-                        array('first' => $beginGraph,'last' => $endGraph)
+                        array('first' => $beginGraph, 'last' => $endGraph)
                     );
                 }
             } else { // Don't show 0 values in the middle of the graph
-                $retn = $app['db']->fetchAll(
+                $dayData = $app['db']->fetchAll(
                     "SELECT * FROM daydata WHERE DATE(datetime) = :date AND NOT kW='0.000'",
                     array('date' => $date)
                 );
             }
         }
-        return $retn;
+        return $dayData;
     }
 
     public function fetchMonth(Silex\Application $app, $month = null, $year = null)
@@ -105,16 +106,16 @@ class Stats
         }
 
         // Setup return variable
-        $retn = array();
+        $monthData = array();
 
         // Check if valid month otherwise we can skip the next part of code
         if ($month > 0 && $month <= 12) {
-            $retn = $app['db']->fetchAll(
+            $monthData = $app['db']->fetchAll(
                 "SELECT date, kW FROM monthdata WHERE MONTH(date) = :m AND YEAR(date) = :y",
                 array('m' => $month, 'y' => $year)
             );
         }
-        return $retn;
+        return $monthData;
     }
 
     public function fetchDayHighcharts(Silex\Application $app, $date = null, $trimZeroData = false)
