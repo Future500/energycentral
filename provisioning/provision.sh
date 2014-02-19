b16a2277487d8dec540adb9e9b2c3548588572a3
Robbin <86378@ict-idcollege.nl>
2014-02-19 13:42:36 +0100

    Added phpMyAdmin to provisioning and updated database file

--- provisioning/provision.sh
+++ provisioning/provision.sh
@@ -3,7 +3,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 # Install dependencies
 apt-get -y update
-apt-get -y install nginx mysql-server mysql-client php5-fpm php5-cli php5-gd php5-mcrypt php5-mysql
+apt-get -y install nginx mysql-server mysql-client phpmyadmin php5-fpm php5-cli php5-gd php5-mcrypt php5-mysql
 
 # Set MySQL root password
 mysqladmin -u root password U7KOekqJOMvf3Uu
@@ -14,4 +14,5 @@ mysql -u root -p"U7KOekqJOMvf3Uu" future500 < "/vagrant/provisioning/db.sql"
 
 # Configure and restart nginx
 sudo -s cp /vagrant/provisioning/nginx.conf /etc/nginx/sites-available/default
+sudo -s /etc/init.d/nginx reload
 sudo -s /etc/init.d/nginx restart
