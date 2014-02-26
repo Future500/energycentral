9cac9ed67ee3a1a124221a7831e256a31a527f64
Robbin <86378@ict-idcollege.nl>
2014-02-26 14:21:00 +0100

    Added select2 to dependencies

--- src/Composerscript.php
+++ src/Composerscript.php
@@ -18,14 +18,17 @@ class Composerscript
 
         $io->write( "Copying images..." );
         `cp vendor/twitter/bootstrap-zip/img/* web/img/`;
+        `cp vendor/ivaynberg/select2/select2-spinner.gif web/img/`;
+        `cp vendor/ivaynberg/select2/select2.png web/img/`;
 
         $io->write( "Copying css..." );
         `cp vendor/twitter/bootstrap-zip/css/* web/css/`;
+        `cp vendor/ivaynberg/select2/select2.css web/css/`;
 
         $io->write( "Copying js..." );
         `cp vendor/twitter/bootstrap-zip/js/* web/js/`;
         `cp vendor/jquery/jquery/* web/js/`;
-
+        `cp vendor/ivaynberg/select2/select2.js web/js/`;
         `cp -r vendor/highcharts/js/* web/js/highcharts/`;
     }
 
