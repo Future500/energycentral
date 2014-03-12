b45525745184da059bad50856a2776b811ffe6d1
Robbin <86378@ict-idcollege.nl>
2014-03-12 09:51:02 +0100

    Updated comment for import command

--- src/console.php
+++ src/console.php
@@ -5,7 +5,7 @@ use Symfony\Component\Console\Application;
 $console = new Application('EnergyCentral', 'n/a');
 $console->add(new EC\Command\CronCommand($app));
 $console->add(new EC\Command\CopyCommand($app));
-$console->add(new EC\Command\ImportCommand($app)); // APPLICATION_ENV="devel-robbin" ./console import:run --keepfiles 2014
+$console->add(new EC\Command\ImportCommand($app)); // APPLICATION_ENV="devel-robbin" ./console import:run --keepfiles 2013
 $console->run();
 
 return $console;
