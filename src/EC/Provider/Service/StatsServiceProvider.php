d2642d60141c4f2aeb6f7e9b9ded6c9608a6afed
rtroost2012 <86378@ict-idcollege.nl>
2014-06-02 14:12:50 +0200

    Added day and month to serviceproviders

--- src/EC/Provider/Service/StatsServiceProvider.php
+++ src/EC/Provider/Service/StatsServiceProvider.php
@@ -3,9 +3,11 @@
 namespace EC\Provider\Service;
 
 use EC;
-use EC\Stats\Month;
+use EC\Service\Month as MonthService;
+use EC\Service\Day as DayService;
+use EC\Service\Stats as StatsService;
 use EC\Stats\Day;
-
+use EC\Stats\Month;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 
@@ -14,7 +16,33 @@ class StatsServiceProvider implements ServiceProviderInterface
     public function register(Application $app)
     {
         $app['stats'] = $app->share(function() use ($app) {
-            return new Device($app['db']);
+            return new StatsService($app['db']);
+        });
+
+        $app['stats.day'] = $app->share(function() use ($app) {
+            return new DayService($app['db']);
+        });
+
+        $app['stats.day.fetch'] = $app->protect(function($deviceId, $date = null, $trimZeroData = false) use ($app) {
+            $dayStats   = new Day($app['stats'], $app['stats.day']);
+            $dayData    = $dayStats
+                ->fetch($deviceId, $date, $trimZeroData)
+                ->getEncodedData();
+
+            return $dayData;
+        });
+
+        $app['stats.month'] = $app->share(function() use ($app) {
+            return new MonthService($app['db']);
+        });
+
+        $app['stats.month.fetch'] = $app->protect(function($deviceId, $month = null, $year = null) use ($app) {
+            $monthStats = new Month($app['stats.month']);
+            $monthData  = $monthStats
+                ->fetch($deviceId, $month, $year)
+                ->getEncodedData();
+
+            return $monthData;
         });
     }
 
