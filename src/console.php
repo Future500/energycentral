3cce7cc5fbf2bebc65bc646d8e7dd6d61eb587f4
rtroost2012 <86378@ict-idcollege.nl>
2014-06-05 11:45:16 +0200

    fixed commands in src/console.php

--- src/console.php
+++ src/console.php
@@ -3,8 +3,8 @@
 use Symfony\Component\Console\Application;
 
 $console = new Application('EnergyCentral', 'n/a');
-$console->add(new EC\Command\CronCommand($app['db'], $app['centralmode']));
-$console->add(new EC\Command\CopyCommand($app['db'], $app['centralmode']));
+$console->add(new EC\Command\CronCommand($app['db']));
+$console->add(new EC\Command\CopyCommand($app['db']));
 $console->add(new EC\Command\ImportCommand($app['db'], $app['centralmode'])); // APPLICATION_ENV="devel-robbin" ./console import:run --keepfiles 2013
 $console->run();
 
